a2978027d46b13a5b79f5f46991619476c63df46
Michelle P. Kuchera <michelle.kuchera@gmail.com>
2021-08-12 10:52:28 +0000

    added compute error group

---
+++ compute_error.py
@@ -0,0 +1,44 @@
+"""
+Computes the percent error versus two determinations 
+of the Hubble constant
+
+name: compute_error.py
+author: Team 6
+date created: 30 June 2021
+
+"""
+
+#imports
+
+
+
+
+# function name: compute_error
+
+    """
+    Computes the percent error of our fit against Hubble's 
+    original computed contstant of H0 = 500 km/s/Mpc and today's c
+    onstant of 70 km/s/Mpc.
+    
+
+    Parameters
+    ----------
+    H0_fit: FLOAT
+        value of Hubble's constant derived from our data in km/s/Mpc
+
+
+    Returns
+    -------
+    error_original: FLOAT
+       percent error with Hubble's original constant as the "true" value
+    error_current: FLOAT
+       percent error with today's Hubble constant as the "true" value   
+    """
+
+
+def main():
+  print("in main")
+
+
+if __name__ == "__main__":
+  main()
