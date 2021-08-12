cfa46fe3e33b22db32f8fd047b9c42bf954485c5
Michelle P. Kuchera <michelle.kuchera@gmail.com>
2021-08-12 10:43:26 +0000

    created plot_results template

---
+++ plot_results.py
@@ -0,0 +1,45 @@
+"""
+Plots scatter data and line of best fit
+
+name: plot_results.py
+author: Team 5
+date created: 30 June 2021
+
+"""
+
+#imports
+
+
+
+# function name: plot_results
+
+    """
+    Plots velocity versus distance data on a scatter plot
+    and plots the line of best fit on top of the data.
+
+    Parameters
+    ----------
+    x: array of FLOAT
+        distance values in *10^6 parsecs.
+    y: array of FLOAT
+        associated velocity data in km/s.
+    m: FLOAT
+        slope of line of best fit.
+    b: FLOAT
+        intercept of line of best fit.
+
+    Returns
+    -------
+    None
+        
+    """
+
+
+
+
+def main():
+  print("in main")
+
+
+if __name__ == "__main__":
+  main()
